842df06dd2f9efa9914193215463eae7a10e9d14
brandon <yangrucheng@gmail.com>
2014-04-06 01:02:19 -0400

    chain of responsibility

---
+++ chainOfResponsibility.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Photo {
+public:
+    Photo(string S) : mTitle(S) {
+        cout << "Processing " << mTitle << "...\n";
+    }
+private:
+    string mTitle;
+};
+
+
+class PhotoProcessor {
+public:
+    PhotoProcessor() : mNextProcessor(0) {}
+
+public:
+    void process(Photo& p) {
+        processImplementation(p);
+        if (mNextProcessor != 0)
+            mNextProcessor->process(p);
+    }
+
+    virtual ~PhotoProcessor() {}
+
+    void setNextProcessor(PhotoProcessor* p){
+        mNextProcessor = p;
+    }
+
+protected:
+    virtual void processImplementation(Photo& a) = 0;
+
+private:
+    PhotoProcessor* mNextProcessor;
+};
+
+class Scale: public PhotoProcessor {
+public:
+    enum SCALE {S50, S100, S200, S300, S500};
+    Scale(SCALE s) : mSCALE(s) {}
+private:
+    void processImplementation(Photo& a) {
+        cout << "Scaling photo\n";
+    }
+
+    SCALE mSCALE;
+};
+
+class RedEye : public PhotoProcessor {
+private:
+    void processImplementation(Photo& a) {
+        cout << "Removing red eye\n";
+    }
+};
+
+class Filter : public PhotoProcessor {
+private:
+    void processImplementation(Photo& a) {
+        cout << "Applying filters\n";
+    }
+};
+
+
+class ColorMatch : public PhotoProcessor {
+private:
+    void processImplementation(Photo& a) {
+        cout << "Mathcing colors\n";
+    }
+};
+
+void processPhoto(Photo& photo) {
+    ColorMatch match;
+    RedEye eye;
+    Filter filter;
+    Scale scale(Scale::S200);
+    scale.setNextProcessor(&eye);
+    eye.setNextProcessor(&match);
+    match.setNextProcessor(&filter);
+    scale.process(photo);
+}
+
+int main() {
+    Photo* p = new Photo("headshot.jpy");
+    processPhoto(*p);
+    return 0;
+}
